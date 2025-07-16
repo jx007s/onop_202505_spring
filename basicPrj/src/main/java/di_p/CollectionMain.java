cfc10af09bee976e570346517fb463899e22d71e
jx007s <jx007s@gmail.com>
2025-07-16 10:56:16 +0900

    7_16_1

--- basicPrj/src/main/java/di_p/CollectionMain.java
+++ basicPrj/src/main/java/di_p/CollectionMain.java
@@ -1,6 +1,7 @@
 package di_p;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,7 +14,25 @@ public class CollectionMain {
 				new ClassPathXmlApplicationContext("qqww/collectionDi.xml");
 		
 		System.out.println(myContext.getBean("arr1"));
-
+		System.out.println(myContext.getBean("ss1"));
+		System.out.println(myContext.getBean("st1"));
+		
+		
+		ArrayList pp1 = new ArrayList();
+		pp1.add("아기상어");
+		pp1.add("엄마상어");
+		pp1.add("아빠상어");
+		pp1.add("할머니상어");
+		pp1.add("할아버지상어");
+		pp1.add("아기상어");
+		
+		ArrayList arr2 = new ArrayList(pp1);
+		HashSet ss2 = new HashSet(pp1); 
+		
+		System.out.println("arr2 : "+arr2);
+		System.out.println("ss2 : "+ss2);
+		
+		
 		myContext.close();
 	}
 
---
+++ basicPrj/src/main/java/di_p/Stud.java
@@ -0,0 +1,21 @@
+package di_p;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Stud {
+
+	String name;
+	int [] jum;
+	
+	
+	
+}
