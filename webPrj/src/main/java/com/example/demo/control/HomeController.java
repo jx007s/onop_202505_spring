9bff8fece622a9a0a79689a2dca40e1f1575b877
jx007s <jx007s@gmail.com>
2025-07-18 16:37:15 +0900

    7_18_4

---
+++ webPrj/src/main/java/com/example/demo/control/HomeController.java
@@ -0,0 +1,51 @@
+package com.example.demo.control;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.Person;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")   //url-mapping 과 같다
+	String home(Model mm) {
+		
+		// request.setAttribute 와 같음
+		mm.addAttribute("now", new Date());
+		mm.addAttribute("arr", new int[] {11,22,33,44});
+		mm.addAttribute("age", 34);
+		mm.addAttribute("per", new Person("이지원",175,false));
+		
+		System.out.println("home 실행");
+		return "index";
+		
+		/*
+		 * 
+application.yml
+
+spring:
+    thymeleaf:
+        prefix: /views/
+        suffix: .html
+        cashe: false
+        
+      "/views/" + "index" + ".html"   으로 forwarding  
+*/
+		
+		
+		
+		
+	}
+}
+
+/*
+////myThProj 프로젝트를 생성하고
+
+/// 학생 데이터(이름,국어,영어,수학,총점,평균)를 출력하세요
+ * */
+
+
